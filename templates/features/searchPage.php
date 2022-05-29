6bc842b00e44b1a31f3d0b6b0077e56a0af427f3
KostyaTarasov <mixtmneon@gmail.com>
2022-05-29 03:23:55 +0300

    Иконки и цвет для пагинации

--- templates/features/searchPage.php
+++ templates/features/searchPage.php
@@ -38,29 +38,29 @@ include __DIR__ . '/../header.php';
         <ul class="pagination justify-content-center">
             <?php if ($previousPageLink != null) : ?>
                 <li class="page-item">
-                    <a class="page-link" href="<?= $previousPageLink ?>">&lt;</a>
+                    <a class="page-link" href="<?= $previousPageLink ?>"><img class="vertical-bottom" src="/images/svg/arrow-left-circle.svg"></a>
                 </li>
             <?php else : ?>
                 <li class="page-item disabled">
-                    <a class="page-link">&lt;</a>
+                    <a class="page-link"><img class="vertical-bottom" src="/images/svg/arrow-left-circle-inactive.svg"></a>
                 </li>
             <?php endif; ?>
             <?php for ($pageNum = 1; $pageNum <= $pagesCount; $pageNum++) : ?>
                 <?php if ($currentPageNum === $pageNum) : ?>
                     <li class="page-item active" aria-current="page">
-                        <a class="page-link"><?= $pageNum ?></a>
+                        <a class="page-link my-pagination-active"><?= $pageNum ?></a>
                     </li>
                 <?php else : ?>
-                    <li class="page-item"><a class="page-link " href="/search/<?= $pageNum ?>"><?= $pageNum ?></a></li>
+                    <li class="page-item"><a class="page-link my-pagination" href="/search/<?= $pageNum ?>"><?= $pageNum ?></a></li>
                 <?php endif; ?>
             <?php endfor; ?>
             <?php if ($nextPageLink !== null) : ?>
                 <li class="page-item">
-                    <a class="page-link" href="<?= $nextPageLink ?>">&gt;</a>
+                    <a class="page-link" href="<?= $nextPageLink ?>"><img class="vertical-bottom" src="/images/svg/arrow-right-circle.svg"></a>
                 </li>
             <?php else : ?>
                 <li class="page-item disabled">
-                    <a class="page-link">&gt;</a>
+                    <a class="page-link"><img class="vertical-bottom" src="/images/svg/arrow-right-circle-inactive.svg"></a>
                 </li>
             <?php endif; ?>
         </ul>
