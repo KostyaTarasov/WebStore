5cb8b495a833ba3355cc877c4d27b0603421f4a8
KostyaTarasov <mixtmneon@gmail.com>
2022-07-06 02:01:09 +0300

    Адаптивные карточки товаров Grid Layout auto-fit

--- templates/catalogs/products.php
+++ templates/catalogs/products.php
@@ -4,35 +4,31 @@ include __DIR__ . '/../header.php';
 ?>
 
 <div>
-    <table class="table2 a-edit">
-        <?php foreach (array_chunk($articles, 4) as $value) : ?>
-            <tr>
-                <?php foreach ($value as $item) : ?>
-                    <td class="td2" onclick="location.href='/catalog/<?= $nameTableCatalog ?>/<?= $item->getId() ?>/'">
-                        <h2 class="td2-text-head text-big">
-                            <a href="/catalog/<?= $nameTableCatalog ?>/<?= $item->getId() ?>/">
-                                <!-- Ccылка на статью для каждого id найденного -->
-                                <?= $item->getName() ?>
-                            </a> <!-- Вывод имени -->
-                        </h2>
-                        <p><?= $item->getParsedText() ?></p> <!-- Вывод основного текста через парсер Markdown-разметки getParsedText(), без парсера getText()-->
-                        <p>Автор: <?= $item->getAuthor()->getNickname() ?></p>
-                        <?php
-                        $image = base64_encode($item->getImage());
-                        if (!empty($image) && $image != "IA==") : ?>
-                            <img class="image small" src="data:image/png;base64, <?= $image ?>">
-                        <?php else : ?>
-                            <img class="image small" src="/images/catalog/no-image.png ">
-                        <?php endif; ?>
-                        <hr>
-                        <p>Цена: <?= $item->getPrice() ?> ₽</p>
-                    </td>
-                <?php endforeach; ?>
-            </tr>
-        <?php endforeach; ?>
-    </table>
+    <div class="products">
+        <ul class="products__list">
+            <?php foreach ($articles as $item) : ?>
+                <li class="products__item a-edit" onclick="location.href='/catalog/<?= $nameTableCatalog ?>/<?= $item->getId() ?>/'">
+                    <h2 class="td2-text-head products__title text-big">
+                        <a href="/catalog/<?= $nameTableCatalog ?>/<?= $item->getId() ?>/">
+                            <!-- Ccылка на статью для каждого id найденного -->
+                            <?= $item->getName() ?>
+                        </a> <!-- Вывод имени -->
+                    </h2>
+                    <p class="margin-null"><?= $item->getParsedText() ?></p> <!-- Вывод основного текста через парсер Markdown-разметки getParsedText(), без парсера getText()-->
+                    <hr class="margin-null" />
+                    <p class="margin-null">Цена: <?= $item->getPrice() ?> ₽</p>
+                    <?php
+                    $image = base64_encode($item->getImage());
+                    if (!empty($image) && $image != "IA==") : ?>
+                        <img class="image small" src="data:image/png;base64, <?= $image ?>">
+                    <?php else : ?>
+                        <img class="image small" src="/images/catalog/no-image.png ">
+                    <?php endif; ?>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    </div>
 </div>
-
 
 <nav aria-label="Пацинация страниц">
     <ul class="pagination justify-content-center">
