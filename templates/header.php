e4ed562967df6250ae2c4de439f1287f6da68c15
KostyaTarasov <mixtmneon@gmail.com>
2022-07-31 22:04:12 +0300

    Добавлена шапка и меню для мобильных устройств

--- templates/header.php
+++ templates/header.php
@@ -8,6 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/../www/styles/styles.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <link rel="shortcut icon" href="/images/personal/favicon.ico" type="image/x-icon">
 </head>
 
@@ -16,29 +17,84 @@
         <div class="site">
             <div class="header sticky-top">
                 <header>
-                    <div class="row header-top">
-                        <div class="col-4">
-                            <div class="text-header text-header-left">
-                                <a href="http://learnphp/contact">Адрес: г.Киров, ул.Ленина 1</a>
-                                <a>С 08-00 до 18-00</a>
+                    <div class="header-top">
+                        <div class="header-mobile">
+                            <div style="display: flex; justify-content: space-between; align-items: center;">
+                                <nav class="navbar navbar-dark">
+                                    <div class="container-fluid">
+                                        <button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarToggleMenu" aria-controls="navbarToggleMenu">
+                                            <span class="navbar-toggler-icon"></span>
+                                        </button>
+                                    </div>
+                                </nav>
+                            </div>
+                            <div class="collapse navigation-menu" id="navbarToggleMenu">
+                                <nav class="navbar navbar-dark">
+                                    <div class="container-fluid">
+                                        <button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarToggleMenu">
+                                            <svg id="close-icon" class="close-icon" width="1.5em" height="1.5em" viewBox="0 0 1024 1024" fill="grey">
+                                                <path d="M866.646 157.397c8.24 8.246 13.332 19.638 13.332 32.217s-5.095 23.972-13.332 32.219v0l-290.179 290.156 290.179 290.179c8.246 8.248 13.352 19.65 13.352 32.235 0 10.944-3.854 20.988-10.287 28.85l0.062-0.081-3.131 3.462c-8.25 8.258-19.654 13.365-32.251 13.365-10.954 0-21.004-3.862-28.864-10.296l0.081 0.062-3.439-3.131-290.179-290.179-290.123 290.179c-8.25 8.256-19.654 13.363-32.251 13.363-25.182 0-45.598-20.413-45.598-45.598 0-12.591 5.099-23.981 13.35-32.235l290.156-290.179-290.156-290.123c-8.258-8.25-13.365-19.654-13.365-32.251 0-10.954 3.862-21.004 10.296-28.864l-0.062 0.081 3.131-3.462c8.25-8.258 19.654-13.365 32.251-13.365 10.954 0 21.004 3.862 28.864 10.296l-0.081-0.062 3.462 3.131 290.123 290.156 290.179-290.156c8.246-8.24 19.638-13.332 32.217-13.332s23.972 5.095 32.219 13.332v0z"></path>
+                                            </svg>
+                                        </button>
+                                    </div>
+                                </nav>
+                                <?php include __DIR__ . '/features/search.php'; ?>
+                                <ul class="navbar-nav navbar-list">
+                                    <div class="dropend">
+                                        <li><a class="dropdown-item navbar-text btn dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">Каталоги</a>
+                                            <ul class="dropdown-menu navigation-menu-catalog">
+                                                <div class="dropstart navigation-menu-catalog-back">
+                                                    <a class="dropdown-item navbar-text dropdown-toggle">Назад</a>
+                                                </div>
+                                                <?php
+                                                foreach ($cpuCatalogs as $value) { ?>
+                                                    <li><a class="dropdown-item navbar-text" href="/catalog/<?= $value->getNameTable() ?>"><?= $value->getName() ?></a></li>
+                                                <?php } ?>
+                                            </ul>
+                                        </li>
+                                    </div>
+                                    <li><a class="dropdown-item navbar-text" href="/../templates/pages/about.php">О себе</a></li>
+                                    <li><a class="dropdown-item navbar-text" href="http://learnphp/contact">Контакты</a></li>
+                                    <li><a class="dropdown-item navbar-text" href="/news">Новости</a></li>
+                                </ul>
+                                <br>
+                                <ul class="navbar-text" style="list-style-type: none; padding-left: 10px;">
+                                    <li>г.Киров, ул.Ленина 1</li>
+                                    <li>Пн. - Пт 08:00 - 18:00</li>
+                                    <li>Сб. - Вс выходной</li>
+                                    <br>
+                                    <li><img class="iconSmall" src="/images/svg/phone1.svg"> 8-800-800-8888</li>
+                                    <li>k.tarasov@yandex.ru</li>
+                                </ul>
                             </div>
                         </div>
-                        <div class="col-4">
-                            <div class="text-header text-header-middle">
-                                <a href="/">Главная</a>
-                                <a href="/../templates/pages/about.php">О себе</a>
-                                <a href="/../manual.php">Документация по PHP</a>
-                                <a href="/news">Новости</a>
-                            </div>
-                        </div>
-                        <div class="col-4">
-                            <div class="text-header text-header-right">
-                                <a><img class="iconSmall" src="/images/svg/phone1.svg"> 8-800-800-8888</a>
+
+                        <div class="header-pc header-top">
+                            <div class="row">
+                                <div class="col-4">
+                                    <div class="text-header text-header-left">
+                                        <a href="http://learnphp/contact">Адрес: г.Киров, ул.Ленина 1</a>
+                                        <a>С 08-00 до 18-00</a>
+                                    </div>
+                                </div>
+                                <div class="col-4">
+                                    <div class="text-header text-header-middle">
+                                        <a href="/">Главная</a>
+                                        <a href="/../templates/pages/about.php">О себе</a>
+                                        <a href="/../manual.php">Документация по PHP</a>
+                                        <a href="/news">Новости</a>
+                                    </div>
+                                </div>
+                                <div class="col-4">
+                                    <div class="text-header text-header-right">
+                                        <a><img class="iconSmall" src="/images/svg/phone1.svg"> 8-800-800-8888</a>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
 
-                    <div class="header-bottom-common">
+                    <div class="header-pc header-bottom-common">
                         <div class="row header-bottom">
                             <div class="col-2">
                                 <a href="/"><img class="logo" src="/images/personal/logo.png "></a>
@@ -57,7 +113,7 @@
 
                             <div class="col-2 underlining">
                                 <?php if (!empty($user)) : ?>
-                                    Привет, <?= $user->getNickname() ?> | <a href="/../users/logOut">Выйти</a>
+                                    <?= $user->getNickname() ?> | <a href="/../users/logOut">Выйти</a>
                                 <?php else : ?>
                                     <a href="/../users/login"><img class="iconSmall" src="/images/svg/person-circle.svg"> Войти</a> | <a href="/../users/register">Регистрация</a>
                                 <?php endif; ?>
