7fe12871d11028b3af66c794e4c264ab0ba6adea
KostyaTarasov <mixtmneon@gmail.com>
2022-08-07 20:46:00 +0300

    Удалено "Найти" из кнопки поиска для min размера

--- templates/features/search.php
+++ templates/features/search.php
@@ -2,7 +2,7 @@
     <div class="input-group">
         <input type="text" class="form-control" name="search" placeholder="Поиск по сайту" value="<?= $_SESSION['postSearch']['search'] ?? '' ?>">
         <button class="btn btn-warning" type="submit">
-            <img class="iconSearch2" src="/images/svg/search.svg"> Найти
+            <img class="iconSearch2" src="/images/svg/search.svg">
         </button>
     </div>
 </form>
