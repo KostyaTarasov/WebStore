75b214cf68db7ac85ce4e76056f8e89406aeb870
KostyaTarasov <mixtmneon@gmail.com>
2022-08-07 21:17:21 +0300

    code style

--- src/routes.php
+++ src/routes.php
@@ -25,17 +25,17 @@ return [
     '~^catalog/(.+)$~' => [\MyProject\Controllers\CatalogController::class, 'firstPage'], // Роут первой страницы определённого каталога товаров
 
     '~^search/(\d+)$~' => [\MyProject\Controllers\SearchController::class, 'page'], // Роут для поиска
-    '~^search(.*)$~' => [\MyProject\Controllers\SearchController::class, 'searchFunction'],                  // Роут для поиска               http://learnphp/search
+    '~^search(.*)$~' => [\MyProject\Controllers\SearchController::class, 'searchFunction'], // Роут для поиска http://learnphp/search
 
-    '~^users/register$~' => [\MyProject\Controllers\UsersController::class, 'signUp'], // Роут для регистрации пользователей                 http://localhost:8080/users/register
+    '~^users/register$~' => [\MyProject\Controllers\UsersController::class, 'signUp'], // Роут для регистрации пользователей http://localhost:8080/users/register
     '~^users/(\d+)/activate/(.+)$~' => [\MyProject\Controllers\UsersController::class, 'activate'], // Роут успешной активации пользователя  Где user_id и code взяты из таблицы users_activation_codes
-    '~^users/login~' => [\MyProject\Controllers\UsersController::class, 'login'],                   // Роут для авторизации пользователя     http://localhost:8080/users/login
-    '~^users/logOut~' => [\MyProject\Controllers\UsersController::class, 'logOut'],                 // Роут для выхода пользователя          http://localhost:8080/users/login
+    '~^users/login~' => [\MyProject\Controllers\UsersController::class, 'login'], // Роут для авторизации пользователя http://localhost:8080/users/login
+    '~^users/logOut~' => [\MyProject\Controllers\UsersController::class, 'logOut'], // Роут для выхода пользователя http://localhost:8080/users/login
 
-    '~^order(.*)$~' => [\MyProject\Controllers\OrderController::class, 'order'],                  // Роут для заказа                http://learnphp/order
-    '~^formOrder(.*)$~' => [\MyProject\Controllers\OrderController::class, 'formOrder'],                  // Роут для оформления заказа             http://learnphp/orderForm
+    '~^order(.*)$~' => [\MyProject\Controllers\OrderController::class, 'order'], // Роут для заказа http://learnphp/order
+    '~^formOrder(.*)$~' => [\MyProject\Controllers\OrderController::class, 'formOrder'],// Роут для оформления заказа http://learnphp/orderForm
 
     '~^news/(\d+)$~' => [\MyProject\Controllers\NewsController::class, 'newsPage'], // Роут новостей
     '~^news(.*)$~' => [\MyProject\Controllers\NewsController::class, 'newsFirstPage'], // Роут первой страницы новостей
-    '~^contact$~' => [\MyProject\Controllers\СontactController::class, 'contact'], // Роут первой страницы новостей
+    '~^contact$~' => [\MyProject\Controllers\СontactController::class, 'contact'], // Роут страницы контактной информации
 ];
